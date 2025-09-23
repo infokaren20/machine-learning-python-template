bbf7fef1a02774479520773dd4075f48f743311f
Karen Paiva <114136329+infokaren20@users.noreply.github.com>
2025-09-23 10:39:59 +0000

    flask

--- src/app.py
+++ src/app.py
@@ -1,16 +1,11 @@
 from flask import Flask, request, render_template
 from pickle import load
 
-
 app = Flask(__name__)
-
-
 model = load(open('naive-bayes-multinomial.pkl', 'rb'))
 vec_model = load(open('naive-bayes-vectorizer.pkl','rb'))
-class_dict = {"0": "It's a negative comment  ):",
-              "1": "Its a positive comment  (:"}
-
-
+class_dict = {"0": "Es un comentario negativo ",
+              "1": "Es un comentario positivo "}
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
@@ -22,9 +17,6 @@ def index():
         pred_class = class_dict[prediction]
         return render_template('index.html', prediction=pred_class)
     return None
-
-
 if __name__ == '__main__':
     # PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=3000, debug=True)
-
